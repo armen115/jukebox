2dc7536f057c34f1edcf976ca631cf46180aace5
armen115 <armenavanesi@live.ca>
2016-09-14 16:53:55 -0400

    Bug fix

--- server.js
+++ server.js
@@ -217,8 +217,8 @@ io.on('connection', function(socket){
 
 })
 
-var networkInterfaces = os.networkInterfaces();
-console.log("Connect to: " + networkInterfaces["wlan0"][0]["address"] + ":" + PORT + " to start adding songs!")
+// var networkInterfaces = os.networkInterfaces();
+// console.log("Connect to: " + networkInterfaces["wlan0"][0]["address"] + ":" + PORT + " to start adding songs!")
 
 http.listen(PORT, function(){
 	console.log('Listening on port 3000')
